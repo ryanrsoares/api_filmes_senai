cd9cd1c534c4096141451ea848ebb681afdc3265
Ryan Rodrigues <ryan.r.soares@aluno.senai.br>
2025-02-20 09:38:15 -0300

    Adicionar arquivos de projeto.

---
+++ Context/Filmes_Context.cs
@@ -0,0 +1,30 @@
+﻿using api_filmes_senai.Domains;
+using Microsoft.EntityFrameworkCore;
+namespace api_filmes_senai.Context
+{
+    public class Filmes_Context : DbContext
+    {
+        public Filmes_Context()
+        {
+        }
+
+        public Filmes_Context(DbContextOptions<Filmes_Context> options) : base(options)
+        {
+        }
+
+        /// <summary>
+        /// Define que as classes se tranformarão em tabelas no bd
+        /// </summary>
+        public DbSet<Genero> Genero { get; set; }
+        public DbSet<Filme> Filme { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server = DESKTOP-0HO9ARA\\SQLEXPRESS; Database = filmes_senai; User Id = sa; Pwd = Senai@134; TrustServerCertificate=true;");
+            }
+        }
+    }
+}
---
+++ Domains/Genero.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api_filmes_senai.Domains
+{
+    public class Genero
+    {
+        [Key]
+        public Guid IdGenero { get; set; }
+
+        [Column(TypeName = "VARCHAR(30)")]
+        [Required(ErrorMessage = "Nome do Gênero é obrigatório!")]
+        public string? Nome { get; set; }
+    }
+}
